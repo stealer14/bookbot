608d94fb6f03c5e9d87e147b2f243920abf04ef5
stealer14 <stealer14@hotmail.com>
2025-03-01 22:20:02 -0600

    adding letter dictionary generator function

--- main.py
+++ main.py
@@ -7,7 +7,8 @@
 # Write a new function called get_book_test that takes a 
 # filepath as input and returns the contents of the 
 # file as a string
-from stats import count_words
+
+from stats import count_words, let_count
 
 def main():
     """
@@ -16,8 +17,11 @@ def main():
     path = './books/frankenstein.txt'
     text = get_book_text(path)
     word_count = count_words(text)
+    word_dict = let_count(text)
+    
     print(word_count)
-
+    print(word_dict)
+    
 def get_book_text(path_to_file):
     """
     Read the contents of a file and return it as a string.
--- stats.py
+++ stats.py
@@ -10,3 +10,16 @@ def count_words(text):
 
     total_count = f"{count} words found in the document"
     return total_count
+
+
+def let_count(text):
+    """
+    Count the number of books in a text.
+    """
+    text = text.split()
+    letter_count = {}
+    for word in text:
+        for letter in word:
+            letter = letter.lower()
+            letter_count[letter] = letter_count.get(letter, 0) + 1
+    return letter_count
